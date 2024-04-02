048006da07977555555ca6149ecd4c6e7a09cdaa
Hurkly <zidni1608@gmail.com>
2024-04-02 13:39:44 +0700

    website template

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Zidni</li> 
+        <li>Nama Belakang : Rizal</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Komplek Marinir blok AA2</li> 
+    </ul> 
+     
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Tanggerang</li> 
+    </ul> 
+ 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Sepeda</li> 
+        <li>Berenang</li> 
+    </ul> 
+</section>
